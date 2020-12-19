f04594ec6ee01990f764726663987534ee508901
esslushy <esslushy@gmail.com>
2020-12-18 22:09:39 -0600

    fixed typo

--- app/routes/report.routes.js
+++ app/routes/report.routes.js
@@ -4,5 +4,5 @@ module.exports = app => {
     const reports = require("../controllers/report.controller");
   
     // Create a new Customer
-    app.post("/customers", reports.create);
+    app.post("/report", reports.create);
 }
